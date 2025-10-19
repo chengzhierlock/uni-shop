85e0c1cb24cb347119e099685c150c37a0b6041b
chengzhierlock <2173452919@qq.com>
2025-10-19 15:59:37 +0800

    完成分类页面开发

--- src/pages/cate/cate.vue
+++ src/pages/cate/cate.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="cate">cate</div>
+  <view class="scroll-view-container">
+    <!-- 左侧滑动 -->
+    <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+      <block v-for="(item, index) in cateList" :key="index">
+        <view :class="['left-scroll-view-item', index === active ? 'active' : '']" @click="activeChanged(index)">{{ item.cat_name }}</view>
+      </block>
+    </scroll-view>
+     <!-- 右侧滑动 -->
+    <scroll-view class="right-scroll-view" scroll-y="true" :scroll-top="scrollTop" :style="{height: wh + 'px'}">
+      <view class="cate-lv2" v-for="(item2, index2) in cateLevel2" :key="index">
+        <view class="cate-lv2-title">{{ item2.cat_name }}</view>
+        <view class="cate-lv3-list">
+          <view class="cate-lv3-item" v-for="(item3, index3) in item2.children" :key="index3" @click="gotoGoodsList(item3)">
+            <image :src="item3.cat_icon"></image>
+            <text>{{ item3.cat_name }}</text>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+  </view>
 </template>
 
 <script lang="ts">
@@ -7,14 +26,49 @@ import {defineComponent} from 'vue';
 export default defineComponent({
   components: {},
   data() {
-    return {}
+    return {
+      // wh为windowheight当前设备可用高度
+      wh: 0,
+      cateList: [],
+      active: 0,
+      cateLevel2: [],
+      scrollTop: 0
+    }
   },
   computed: {},
-  methods: {},
+  methods: {
+    async getCateList() {
+      const {data: res} = await uni.$http.get('/api/public/v1/categories')
+      // console.log(res);
+      if(res.meta.status !== 200) return uni.$showMsg();
+      this.cateList = res.message;
+      this.cateLevel2 = res.message[0].children || [];
+      console.log(this.cateLevel2);
+      
+    },
+    activeChanged(i) {
+      // console.log(i);
+      this.active = i
+      this.cateLevel2 = this.cateList[i].children || []
+      this.scrollTop = this.scrollTop === 0 ? 1 : 0  
+    },
+
+    gotoGoodsList(item3) {
+      console.log(item3);
+      
+      uni.navigateTo({
+        url:'/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+      })
+    }
+  },
   watch: {},
 
   // 页面周期函数--监听页面加载
-  onLoad() {},
+  onLoad() {
+    const sysInfo = uni.getSystemInfoSync();
+    this.wh = sysInfo.windowHeight;
+    this.getCateList();
+  },
   // 页面周期函数--监听页面初次渲染完成
   onReady() {},
   // 页面周期函数--监听页面显示(not-nvue)
@@ -34,4 +88,63 @@ export default defineComponent({
 }) 
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+  .scroll-view-container {
+    display: flex;
+    .left-scroll-view {
+      width: 120px;
+      .left-scroll-view-item {
+        background-color: #F7F7F7;
+        line-height: 60px;
+        text-align: center;
+        font-size: 12px;
+
+        &.active {
+          background-color: #FFF;
+          position: relative;
+
+            &::before {
+            content: ' ';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #C00000;
+            position: absolute;
+            top:50%;
+            left: 0;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
+    .cate-lv2-title {
+      font-size: 12px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px 0;
+    }
+
+    .cate-lv3-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .cate-lv3-item {
+      width: 33.33%;
+      margin-bottom: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      image {
+        width: 60px;
+        height: 60px;
+      }
+
+      text {
+        font-size: 12px;
+      }
+    }
+  }
+
+  }
+</style>
